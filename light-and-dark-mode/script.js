23df08c29d7aa6e3300e9db9a250f09daabe82db
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-26 01:01:54 +0530

    used browser local storage to store the color mode

--- light-and-dark-mode/script.js
+++ light-and-dark-mode/script.js
@@ -3,6 +3,10 @@ const innerText = document.querySelector(".inner-text");
 const outerCheckbox = document.querySelector("#checkbox-outer");
 const innerCheckbox = document.querySelector("#checkbox-inner");
 
+const darkmode = JSON.parse(localStorage.getItem("darkmode")) || {
+  outer: false,
+  inner: false
+}
 
 const changeBody = function (color, backgroundColor) {
   body.style.color = color;
@@ -14,23 +18,47 @@ const changeInnerText = function (color, backgroundColor) {
   innerText.style.backgroundColor = backgroundColor;
 }
 
+if (darkmode.inner) {
+  changeInnerText("white", "black");
+  innerCheckbox.checked = true;
+}
+if (darkmode.outer) {
+  changeBody("white", "black");
+  changeInnerText("white", "black");
+  outerCheckbox.checked = true;
+  innerCheckbox.checked = true;
+}
+
+if (!darkmode.inner) {
+  changeInnerText("black", "white");
+  innerCheckbox.checked = false;
+}
+
 outerCheckbox.addEventListener("change", function () {
   if (outerCheckbox.checked) {
     changeBody("white", "black");
     changeInnerText("white", "black");
     innerCheckbox.checked = true;
+    darkmode.inner = darkmode.outer = true;
+    localStorage.setItem("darkmode", JSON.stringify(darkmode));
   } else {
     changeBody("black", "white");
     changeInnerText("black", "white");
     innerCheckbox.checked = false;
+    darkmode.inner = darkmode.outer = false;
+    localStorage.setItem("darkmode", JSON.stringify(darkmode));
   }
 });
 
 innerCheckbox.addEventListener("change", function () {
   if (innerCheckbox.checked) {
     changeInnerText("white", "black");
+    darkmode.inner = true;
+    localStorage.setItem("darkmode", JSON.stringify(darkmode));
   } else {
     changeInnerText("black", "white");
+    darkmode.inner = false;
+    localStorage.setItem("darkmode", JSON.stringify(darkmode));
   }
 });
 
